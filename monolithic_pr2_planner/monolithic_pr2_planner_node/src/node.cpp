6b69da838798bec1825b8c7671f88088a37aa251
Fahad Islam <fahad@lionturtle.(none)>
2014-10-04 15:49:34 -0400

    monolithic planner simulation version to copy to the robot

--- monolithic_pr2_planner/monolithic_pr2_planner_node/src/node.cpp
+++ monolithic_pr2_planner/monolithic_pr2_planner_node/src/node.cpp
@@ -137,32 +137,32 @@ int main(int argc, char** argv){
     ROS_INFO("Node is ready to receive planning requests.");
 
 
-    ros::Publisher vis_pub = n.advertise<visualization_msgs::Marker>( "visualization_marker", 1 );
-    sleep(2);
-    visualization_msgs::Marker marker;
+    // ros::Publisher vis_pub = n.advertise<visualization_msgs::Marker>( "visualization_marker", 1 );
+    // sleep(2);
+    // visualization_msgs::Marker marker;
 
-    marker.header.frame_id = "/map";
-    marker.header.stamp = ros::Time::now();
-    marker.ns = "my_namespace";
-    marker.id = 0;
-    marker.type = visualization_msgs::Marker::MESH_RESOURCE;
-    marker.action = visualization_msgs::Marker::ADD;
-    marker.pose.position.x = 0;
-    marker.pose.position.y = 0;
-    marker.pose.position.z = 0;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
-    marker.scale.x = 1.0;
-    marker.scale.y = 1.0;
-    marker.scale.z = 1.0;
-    marker.color.a = 1.0;
-    marker.color.r = 0.3;
-    marker.color.g = 0.3;
-    marker.color.b = 0.3;
-    //only if using a MESH_RESOURCE marker type:
-    marker.mesh_resource = "package://monolithic_pr2_planner_node/maps/office/max_kitchen.dae";
-    vis_pub.publish( marker );
+    // marker.header.frame_id = "/map";
+    // marker.header.stamp = ros::Time::now();
+    // marker.ns = "my_namespace";
+    // marker.id = 0;
+    // marker.type = visualization_msgs::Marker::MESH_RESOURCE;
+    // marker.action = visualization_msgs::Marker::ADD;
+    // marker.pose.position.x = 0;
+    // marker.pose.position.y = 0;
+    // marker.pose.position.z = 0;
+    // marker.pose.orientation.x = 0.0;
+    // marker.pose.orientation.y = 0.0;
+    // marker.pose.orientation.z = 0.0;
+    // marker.pose.orientation.w = 1.0;
+    // marker.scale.x = 1.0;
+    // marker.scale.y = 1.0;
+    // marker.scale.z = 1.0;
+    // marker.color.a = 1.0;
+    // marker.color.r = 0.3;
+    // marker.color.g = 0.3;
+    // marker.color.b = 0.3;
+    // //only if using a MESH_RESOURCE marker type:
+    // marker.mesh_resource = "package://monolithic_pr2_planner_node/maps/office/max_kitchen.dae";
+    // vis_pub.publish( marker );
     ros::spin();
 }
